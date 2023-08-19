27e95a7a4bed0f90b40a9a1f23fd8e9cc5c24d04
Yelshat Duskaliyev <ura2178@gmail.com>
2023-08-20 02:30:17 +0600

    feat: added function to load tickets tree by PRs

--- app/service/notes/eval.go
+++ app/service/notes/eval.go
@@ -3,23 +3,32 @@ package notes
 import (
 	"context"
 	"fmt"
+	"github.com/Semior001/releaseit/app/git"
 	"github.com/Semior001/releaseit/app/task"
+	tengine "github.com/Semior001/releaseit/app/task/engine"
+	"github.com/samber/lo"
+	"regexp"
 	"sort"
 	"text/template"
 )
 
 // EvalAddon is an addon to evaluator, to be used in release notes template.
-type EvalAddon struct{}
+type EvalAddon struct {
+	TaskTracker *tengine.Tracker
+}
 
 // String returns addon name.
 func (e *EvalAddon) String() string { return "release-notes" }
 
 // Funcs returns a map of functions to be used in release notes template.
-func (e *EvalAddon) Funcs(_ context.Context) (template.FuncMap, error) {
-	return template.FuncMap{"treeTickets": e.treeTickets}, nil
+func (e *EvalAddon) Funcs(ctx context.Context) (template.FuncMap, error) {
+	return template.FuncMap{
+		"buildTicketsTree": e.buildTicketsTree,
+		"loadTicketsTree":  e.loadTicketsTree(ctx),
+	}, nil
 }
 
-func (e *EvalAddon) treeTickets(tickets []task.Ticket) (roots []*TicketNode, err error) {
+func (e *EvalAddon) buildTicketsTree(tickets []task.Ticket) (roots []*TicketNode, err error) {
 	ticketMap := make(map[string]*TicketNode)
 	for _, ticket := range tickets {
 		ticketMap[ticket.ID] = &TicketNode{Ticket: ticket}
@@ -39,22 +48,73 @@ func (e *EvalAddon) treeTickets(tickets []task.Ticket) (roots []*TicketNode, err
 		parent.Children = append(parent.Children, ticket)
 	}
 
-	var sortTickets func(nodes []*TicketNode)
-	sortTickets = func(nodes []*TicketNode) {
-		sort.Slice(nodes, func(i, j int) bool {
-			return nodes[i].ID < nodes[j].ID
-		})
-		for _, node := range nodes {
-			sortTickets(node.Children)
-		}
-	}
-	sortTickets(roots)
+	sortTicketNodes(roots)
 
 	return roots, nil
+}
+
+func (e *EvalAddon) loadTicketsTree(ctx context.Context) func(string, bool, []git.PullRequest) (LoadedTree, error) {
+	return func(ticketIDRx string, loadParents bool, prs []git.PullRequest) (LoadedTree, error) {
+		rx, err := regexp.Compile(ticketIDRx)
+		if err != nil {
+			return LoadedTree{}, fmt.Errorf("compile regexp: %w", err)
+		}
+
+		ticketPRs := map[string][]git.PullRequest{} // ticketID -> PR index
+		var unattached []git.PullRequest
+		for _, pr := range prs {
+			submatches := rx.FindAllStringSubmatch(pr.Title, -1)
+			if len(submatches) == 0 {
+				unattached = append(unattached, pr)
+				continue
+			}
+
+			for _, submatch := range submatches {
+				ticketID := submatch[1]
+				ticketPRs[ticketID] = append(ticketPRs[ticketID], pr)
+			}
+		}
+
+		tickets, err := e.TaskTracker.List(ctx, lo.Keys(ticketPRs), loadParents)
+		if err != nil {
+			return LoadedTree{}, fmt.Errorf("load tickets: %w", err)
+		}
+
+		tree, err := e.buildTicketsTree(tickets)
+		if err != nil {
+			return LoadedTree{}, fmt.Errorf("build tickets tree: %w", err)
+		}
+		addPRs(ticketPRs, tree)
+
+		return LoadedTree{Roots: tree, Unattached: unattached}, nil
+	}
+}
+
+func sortTicketNodes(nodes []*TicketNode) {
+	sort.Slice(nodes, func(i, j int) bool { return nodes[i].ID < nodes[j].ID })
+	for _, node := range nodes {
+		sortTicketNodes(node.Children)
+	}
+}
+
+func addPRs(ticketPRs map[string][]git.PullRequest, nodes []*TicketNode) {
+	for _, node := range nodes {
+		if prs, ok := ticketPRs[node.ID]; ok {
+			node.PRs = append(node.PRs, prs...)
+		}
+		addPRs(ticketPRs, node.Children)
+	}
+}
+
+// LoadedTree is a tree of tickets with their children and PRs.
+type LoadedTree struct {
+	Roots      []*TicketNode
+	Unattached []git.PullRequest
 }
 
 // TicketNode is a representation of a ticket with its children.
 type TicketNode struct {
 	task.Ticket
 	Children []*TicketNode
+	PRs      []git.PullRequest
 }
